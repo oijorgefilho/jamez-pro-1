5055922bb0f5850d785b4e63bdf26d182555027d
oijorgefilho <oijorgefilho@gmail.com>
2025-01-21 12:37:24 -0300

    Fix: Adiciona providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,6 +2,8 @@
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { ReactNode } from 'react'
+import { ToastProvider } from '@/components/ui/toast'
+import { Toaster } from '@/components/ui/toaster'
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
@@ -12,7 +14,10 @@ export function Providers({ children }: { children: ReactNode }) {
       forcedTheme="dark"
       disableTransitionOnChange
     >
-      {children}
+      <ToastProvider>
+        {children}
+        <Toaster />
+      </ToastProvider>
     </NextThemesProvider>
   )
 }
